285af1b6dfecf64c14e5c1cb638f0fddcb1008fc
FELIPE TORRES GONZALEZ <felipe.gonzalez@etec.sp.gov.br>
2025-08-25 16:26:47 -0300

    criaçao de todas as rotas

---
+++ src/app.js
@@ -0,0 +1,51 @@
+const express = require ('express');
+const app = express();
+
+app.post('/animais', (request, responde) => {
+
+})
+
+app.post('/tutores', (request, responde) => {
+
+})
+
+app.get('/animais', (request, responde) => {
+
+})
+
+app.patch('/tutores:id', (request, responde) => {
+
+})
+
+app.get('/tutores:id', (request, responde) => {
+
+})
+app.post('/questionario', (request, responde) => {
+
+})
+app.post('/adocoes', (request, responde) => {
+
+})
+app.get('/admin/animais', (request, responde) => {
+
+})
+
+app.patch('/admin/animais:id', (request, responde) => {
+
+})
+
+app.delete('/admin/animais:id', (request, response) => {
+
+})
+
+app.get('/animais:id', (request, response) => {
+
+})
+
+app.post('/login', (request, response) => {
+
+})
+
+app.post('doacoes', (request, response) => {
+    
+})
