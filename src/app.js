3592df36095e8b109a8239dc8ba9ba1eade372a4
lucasrodrigues117 <rodrigues.lds.lfr@gmail.com>
2025-08-25 17:31:53 -0300

    teste3

--- src/app.js
+++ src/app.js
@@ -154,5 +154,5 @@ app.post('doacoes', (request, response) => {
 })
 
 app.listen(3333, () => {
-    console.log(`🚀 servidor rodando na port ${port} teste`)
+    console.log(`🚀 servidor rodando na port ${port} teste2`)
 })
